506a04d4f5653bb0e1253beb432079f1fcd83e28
jayanthamara21 <jayanth.amara@prodapt.com>
2026-07-17 17:23:59 +1300

    Add example of primitive and non-primitive data types

---
+++ DAY 10/js/example2.js
@@ -0,0 +1,23 @@
+//Primitive Data Types
+let name = "John" //String
+let age = 30 //Number
+let isActive = true //Boolean
+let address; //Undefined
+let salary = null //Null
+let id = Symbol("id") //Symbol
+let big = 123456789n //BigInt
+
+//Non Primitive
+let student = {
+    name: "John",
+    age: 30
+};
+
+let colors = ["red", "green"];
+
+function greet() {
+    console.log("Hello");
+}
+
+greet();
+
